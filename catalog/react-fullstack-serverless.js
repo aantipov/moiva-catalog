cba42431afc4832693931b43e8ea247a359e9a56
Alexey Antipov <antipov.alexei@gmail.com>
2021-01-25 08:50:08 +0100

    add fullstask-serverless and testing-bdd categories (#17)

--- catalog/react-fullstack-serverless.js
+++ catalog/react-fullstack-serverless.js
@@ -1,19 +1,12 @@
 const libs =  {
-  name: 'Vue Component Libraries',
+  name: 'Fullstack Serverless React Frameworks',
   items: [
     // <npm-package-name> or [<npm-package-name>, <seo-alias>]
     // Seo aliases are used to refer to npm packages in Google Search results. For example, for 'angular/core' Google will show results with a title "Angular: Stats and Trends - Moiva.io".
     // Aliases are important to help users find the right page
     // Important: when adding an alias, make sure it's unique - no other packages in this file should use it as their name
-    'vuetify',
-    'quasar',
-    ['bootstrap-vue', 'BootstrapVue'],
-    'primevue',
-    ['vue-material', 'Vue Material'],
-    ['element-ui', 'Element UI'],
-    ['element-plus', 'Element Plus'],
-    ['iview', 'iView'],
-    ['ant-design-vue', 'Ant Design Vue'],
+    ['blitz', null, 'react'],
+    ['@redwoodjs/core', 'RedwoodJS', 'react', 'redwoodjs/redwood']
   ]
 };
 
